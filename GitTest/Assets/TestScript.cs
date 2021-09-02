7c8cf8eb9741608a349777ee05c24263d5ad7053
yashir333 <51927131+yashir333@users.noreply.github.com>
2021-09-02 15:40:50 +0300

    Update TestScript.cs
    The script changde

--- GitTest/Assets/TestScript.cs
+++ GitTest/Assets/TestScript.cs
@@ -7,7 +7,8 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //This scrpit changed
+
     }
 
     // Update is called once per frame
